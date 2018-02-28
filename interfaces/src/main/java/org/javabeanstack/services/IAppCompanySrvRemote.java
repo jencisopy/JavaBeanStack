b2a1ac2a61d97610803a89bf9403e7bf871db6fb
Jorge Enciso <jorge.enciso.r@gmail.com>
2018-02-28 08:01:57 -0400

    MK-301 Interface servicio Empresa

--- interfaces/src/main/java/org/javabeanstack/services/IAppCompanySrvRemote.java
+++ interfaces/src/main/java/org/javabeanstack/services/IAppCompanySrvRemote.java
@@ -21,19 +21,10 @@
 */
 package org.javabeanstack.services;
 
-import java.io.Serializable;
-import java.util.Map;
-import org.javabeanstack.data.IDataRow;
-import org.javabeanstack.data.IGenericDAO;
-import org.javabeanstack.error.IErrorReg;
-
 /**
  *
  * @author Jorge Enciso
  */
-public interface IDataService extends IGenericDAO, Serializable{
-    <T extends IDataRow> T setListFieldCheck(T row); 
-    <T extends IDataRow> boolean checkUniqueKey(String sessionId, T row) throws Exception;        
-    <T extends IDataRow> boolean checkForeignKey(String sessionId, T row, String fieldName) throws Exception;            
-    <T extends IDataRow> Map<String, IErrorReg> checkDataRow(String sessionId, T row) ;    
+public interface IAppCompanySrvRemote extends IAppCompanySrv {
+    
 }
