1d4214a5232bdde6de0f916fffd2e896be054418
JORGE <jorge.enciso.r@gmail.com>
2022-08-04 11:50:01 -0400

    Seguridad de accesos

--- interfaces/src/main/java/org/javabeanstack/security/IAppObjectAuthSrv.java
+++ interfaces/src/main/java/org/javabeanstack/security/IAppObjectAuthSrv.java
@@ -33,6 +33,7 @@ import org.javabeanstack.model.IAppUser;
 public interface IAppObjectAuthSrv extends IDataService {
     Integer checkAuthUserOnly(String sessionId, Long idAppObject, IAppUser user, String action, Integer authDenyDefault);    
     Integer checkAuth(String sessionId, Long idAppObject, Long iduser, String action, Map<String, String> checkResult, Integer authDenyDefault);
+    Integer checkAuth(String sessionId, Long idAppObject, IAppUser user, String action, Map<String, String> checkResult, Integer authDenyDefault);        
     boolean checkAuthField(Long idAppObject, Long iduser, String field, String action);
     String getAuthXml(Long idAppObject, Long iduser);
     IAppObjectAuth getAppObjectAuthList();
